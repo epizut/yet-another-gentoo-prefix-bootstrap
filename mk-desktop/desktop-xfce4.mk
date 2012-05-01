f6ae028f71ff3a4ee0eefa9eab97070f14345d91
epimerde <epimerde@gmail.com>
2012-05-01 11:56:57 +0200

    Moving desktop files into a specific directory

---
+++ mk-desktop/desktop-xfce4.mk
@@ -0,0 +1,14 @@
+ifndef DESKTOP-XFCE4_MK
+SYSTEM_MK=desktop-xfce4.mk
+
+include init.mk
+
+desktop-xfce4: system \
+	desktop-xfce4-base
+
+
+desktop-xfce4-base:
+	${EMERGE} -uN xfce-base/xfce4-meta
+
+
+endif
